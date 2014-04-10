e8c30cbed396ba8f2ae2977683f1061fd6cab649
ArkEidolon <clarke.plumo@gmail.com>
2014-04-10 09:57:30 +0800

    Initial commit.

---
+++ FuzzyLogic/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMax = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbTrapezoids = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtMax
+            // 
+            this.txtMax.Location = new System.Drawing.Point(145, 264);
+            this.txtMax.Name = "txtMax";
+            this.txtMax.Size = new System.Drawing.Size(43, 20);
+            this.txtMax.TabIndex = 0;
+            this.txtMax.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtMax_KeyDown);
+            this.txtMax.Leave += new System.EventHandler(this.txtMax_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 267);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Trapezoids:";
+            // 
+            // lbTrapezoids
+            // 
+            this.lbTrapezoids.Font = new System.Drawing.Font("Consolas", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTrapezoids.FormattingEnabled = true;
+            this.lbTrapezoids.ItemHeight = 10;
+            this.lbTrapezoids.Location = new System.Drawing.Point(16, 20);
+            this.lbTrapezoids.Name = "lbTrapezoids";
+            this.lbTrapezoids.Size = new System.Drawing.Size(172, 234);
+            this.lbTrapezoids.TabIndex = 2;
+            this.lbTrapezoids.SelectedIndexChanged += new System.EventHandler(this.lbTrapezoids_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(194, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(283, 74);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Trapezoid # 000";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(203, 23);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(68, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Crisp Output:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(120, 46);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(40, 20);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(96, 49);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "D:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(196, 46);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(75, 20);
+            this.textBox5.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(120, 20);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(40, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(96, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "C:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(31, 46);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(40, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "B:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(31, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(40, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "A:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(194, 94);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 190);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(510, 56);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(38, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Inc";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(554, 56);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(40, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Dec";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(554, 33);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(40, 20);
+            this.textBox6.TabIndex = 10;
+            this.textBox6.Text = "100";
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(530, 36);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "X:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 294);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lbTrapezoids);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMax);
+            this.Name = "Form1";
+            this.Text = "Fuzzy Logic";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMax;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lbTrapezoids;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ FuzzyLogic/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        ArrayList trapezoids;
+        public static float x;
+
+        public Form1()
+        {
+            InitializeComponent();
+            trapezoids = new ArrayList();
+            txtMax.Focus();
+            txtMax.Text = "1";
+            txtMax_Leave(null, null);
+            lbTrapezoids.SelectedIndex = 0;
+        }
+
+        private void txtMax_Leave(object sender, EventArgs e)
+        {
+            try
+            {
+                int x = int.Parse(txtMax.Text);
+
+                while (trapezoids.Count < x)
+                    trapezoids.Add(new Trapezoid(trapezoids.Count+1, 0, 0, 0, 0));
+
+                while (trapezoids.Count > x)
+                    trapezoids.RemoveAt(trapezoids.Count - 1);
+
+                RefreshGrid();
+            }
+            catch { }
+        }
+
+        private void txtMax_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == '\r')
+            {
+                groupBox1.Focus();
+            }
+        }
+
+        private void lbTrapezoids_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Trapezoid trapezoid = lbTrapezoids.SelectedItem as Trapezoid;
+            groupBox1.Text = string.Format("Trapezoid # {0}", trapezoid.id.ToString("000"));
+            textBox1.Text = trapezoid.a.ToString();
+            textBox2.Text = trapezoid.b.ToString();
+            textBox3.Text = trapezoid.c.ToString();
+            textBox4.Text = trapezoid.d.ToString();
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Trapezoid trapezoid = lbTrapezoids.SelectedItem as Trapezoid;
+            trapezoid.a = float.Parse(textBox1.Text);
+            RefreshGrid();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            Trapezoid trapezoid = lbTrapezoids.SelectedItem as Trapezoid;
+            trapezoid.b = float.Parse(textBox2.Text);
+            RefreshGrid();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            Trapezoid trapezoid = lbTrapezoids.SelectedItem as Trapezoid;
+            trapezoid.c = float.Parse(textBox3.Text);
+            RefreshGrid();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            Trapezoid trapezoid = lbTrapezoids.SelectedItem as Trapezoid;
+            trapezoid.d = float.Parse(textBox4.Text);
+            RefreshGrid();
+        }
+
+        static Bitmap bitmap = new Bitmap(400, 190);
+        Graphics graphics = Graphics.FromImage(bitmap);
+        Pen pen = new Pen(Brushes.Black, 4);
+
+        public void RefreshGrid()
+        {
+            Form1.x = float.Parse(textBox6.Text);
+
+            int selected = lbTrapezoids.SelectedIndex;
+            lbTrapezoids.Items.Clear();
+            foreach (Trapezoid trapezoid in trapezoids)
+                lbTrapezoids.Items.Add(trapezoid);
+
+            lbTrapezoids.SelectedIndex = selected;
+
+            textBox5.Text = (lbTrapezoids.SelectedItem as Trapezoid).CrispOutput(float.Parse(textBox6.Text)).ToString();//(float.Parse(textBox6.Text));
+            graphics.Clear(Color.White);
+
+            graphics.DrawLine(new Pen(Brushes.Black, 10), 0, 180, 490, 180);
+            graphics.DrawLine(new Pen(Brushes.Black, 10), 180-Form1.x, 190, 180-Form1.x, 0);
+
+            foreach (Trapezoid trapezoid in lbTrapezoids.Items)
+            {
+                graphics.DrawLine(pen, 180 + trapezoid.a - x, 180, 180 + trapezoid.b - x, 90);
+                graphics.DrawLine(pen, 180 + trapezoid.b - x, 90, 180 + trapezoid.c - x, 90);
+                graphics.DrawLine(pen, 180 + trapezoid.c - x, 90, 180 + trapezoid.d - x, 180);
+
+                graphics.DrawString(trapezoid.a.ToString(), new Font(FontFamily.GenericSansSerif, 8), Brushes.Cyan, 160 + trapezoid.a-x, 173);
+                graphics.DrawString(trapezoid.b.ToString(), new Font(FontFamily.GenericSansSerif, 8), Brushes.Blue, 160 + trapezoid.b-x, 80);
+                graphics.DrawString(trapezoid.c.ToString(), new Font(FontFamily.GenericSansSerif, 8), Brushes.Blue, 180 + trapezoid.c - x, 80);
+                graphics.DrawString(trapezoid.d.ToString(), new Font(FontFamily.GenericSansSerif, 8), Brushes.Cyan, 180 + trapezoid.d - x, 173);
+            }
+
+            try
+            {
+                float x = float.Parse(textBox6.Text);
+                graphics.DrawLine(pen, 180, 0, 180, 190);
+                graphics.DrawString(x.ToString(), new Font(FontFamily.GenericSansSerif, 8), Brushes.Black, 180, 0);
+            }
+            catch {}
+            pictureBox1.Refresh();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.BackgroundImage = bitmap;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RefreshGrid();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox6.Text = (float.Parse(textBox6.Text) + 1).ToString();
+            RefreshGrid();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox6.Text = (float.Parse(textBox6.Text) - 1).ToString();
+            RefreshGrid();
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            RefreshGrid();
+        }
+    }
+
+    class Trapezoid
+    {
+        public int id;
+        public float a;
+        public float b;
+        public float c;
+        public float d;
+
+        public Trapezoid(int id, float a, float b, float c, float d)
+        {
+            this.id = id;
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.d = d;
+        }
+
+        public float CrispOutput(float x)
+        {
+            return Max(Min((x-a)/(b-a), 1, (d-x)/(d-c)), 0);
+        }
+        public override string ToString()
+        {
+            return string.Format("{0}   {1,3}, {2,3}, {3,3}, {4,3}   {5}", id.ToString("000"), a, b, c, d, (CrispOutput(Form1.x)).ToString("0.00"));
+        }
+
+        public float Min(params float[] values)
+        {
+            float min = values[0];
+
+            for (int i = 1; i < values.Length; i++)
+                if (values[i] < min)
+                    min = values[i];
+
+            return min;
+        }
+
+        public float Max(params float[] values)
+        {
+            float max = values[0];
+
+            for (int i = 1; i < values.Length; i++)
+                if (values[i] > max)
+                    max = values[i];
+
+            return max;
+        }
+    }
+}
